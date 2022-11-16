deff35775794b65e2151527a3e6bf6e5e61469d0
meo2718 <agv1291.@gmail.com>
2022-11-17 02:38:08 +0900

    サービスプロバイダーのテスト

--- umarche/routes/web.php
+++ umarche/routes/web.php
@@ -25,4 +25,5 @@ Route::get('/dashboard', function () {
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
 Route::get('/servicecontainer-test', [LifeCycleTestController::class, 'showServiceControllerTest']);
+Route::get('/serviceprovider-test', [LifeCycleTestController::class, 'showServiceProviderTest']);
 require __DIR__.'/auth.php';
