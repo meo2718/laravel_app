f4bce727390f001a381094d8c434799e1cdff3ec
meo2718 <agv1291.@gmail.com>
2022-11-17 20:33:46 +0900

    [Add]RouteServiceProvider #15

--- umarche/app/Providers/RouteServiceProvider.php
+++ umarche/app/Providers/RouteServiceProvider.php
@@ -14,10 +14,13 @@ class RouteServiceProvider extends ServiceProvider
      * The path to the "home" route for your application.
      *
      * This is used by Laravel authentication to redirect users after login.
-     *
+     * User,Admin,Ownerそれぞれのログイン後のリダイレクト先パスの設定
      * @var string
      */
     public const HOME = '/dashboard';
+    public const OWNER_HOME = '/owner/dashboard'; 
+    public const ADMIN_HOME = '/admin/dashboard';
+
 
     /**
      * The controller namespace for the application.
@@ -30,7 +33,10 @@ class RouteServiceProvider extends ServiceProvider
 
     /**
      * Define your route model bindings, pattern filters, etc.
-     *
+     * ルート情報を設定する。bootメソッド→サービスプロバロバイダが読み込まれたあとに実行されるメソッド
+     * ルートパターンはミドルウェアのap1とwebを使う２パターン
+     * laravelでviewを表示してリクエストレスポンスを返すパターンは→web
+     * フロントをすべてjsなどで作る場合はapiを使う
      * @return void
      */
     public function boot()
@@ -38,14 +44,35 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
+            //prefix→URLの頭にapiとつける。Routeはファサード内のメソッドを使ってる
             Route::prefix('api')
+            //ミドルウェアをグループのすべてのルートに割り当てる
                 ->middleware('api')
                 ->namespace($this->namespace)
+            //ヘルパ関数base_pathでroutes/api.phpのすべてのURLにapiがつく
                 ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
+            
+            //prefix('/')でownerやadminがついてないURLは/へ遷移させる
+            Route::prefix('/')
+            //asをつけることで別名にできる'/'→'user.'に変わる
+                ->as('user.')
+                ->middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+           
+            //owner用
+            Route::prefix('owner')
+                ->as('owner.')
+                ->middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/owner.php'));
+            
+            //admin用
+            Route::prefix('admin')
+                ->as('admin.')
+                ->middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
         });
     }
 
