16f4d451e5202db789347629c587829bdf109dc4
meo2718 <agv1291.@gmail.com>
2022-11-20 18:12:15 +0900

    [Add]User,Adminコントローラー設定#15

--- umarche/routes/admin.php
+++ umarche/routes/admin.php
@@ -27,7 +27,8 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    //guard権限:を付与、→adminの権限を持ってたらダッシュボードへ
+})->middleware(['auth:admin'])->name('dashboard');
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
@@ -53,7 +54,7 @@ Route::middleware('guest')->group(function () {
                 ->name('password.update');
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth:admin')->group(function () {
     Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
                 ->name('verification.notice');
 
