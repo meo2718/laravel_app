3b104dc53b1365c68d0148de16a38c72f9d91427
meo2718 <agv1291@gmail.com>
2022-12-15 05:13:58 +0900

    [Add]imageのリレーション #45

--- umarche/app/Models/Owner.php
+++ umarche/app/Models/Owner.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Shop;
+use App\Models\Image;
 
 class Owner extends Authenticatable
 {
@@ -45,5 +46,11 @@ class Owner extends Authenticatable
     public function shop()
     {
         return $this->hasOne(Shop::class);
+    }
+
+    public function image()
+    {
+        //imageを複数もてるのでhasManyとする
+        return $this->hasMany(Image::class);
     }
 }
