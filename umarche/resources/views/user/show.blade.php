9f912f1c9cc804138e3cc6b2035121ce6213a21b
meo2718 <agv1291@gmail.com>
2023-01-02 23:28:32 +0900

    [Add]userShow画面の調整 #54

--- umarche/resources/views/user/show.blade.php
+++ umarche/resources/views/user/show.blade.php
@@ -9,7 +9,41 @@
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 bg-white border-b border-gray-200">
-                  {{ $product->name }}
+
+                 <div class="md:flex md:justify-around">
+                     {{-- 商品画像表示 --}}
+                     <div class="md:w-1/2">
+                         <x-thumbnail filename="{{$product->imageFirst->filename ?? ''}}" type="products" />
+                     </div>
+                     {{-- カテゴリー名、商品名、商品情報表示 --}}
+                     <div class="md:w-1/2 ml-4">
+                         <h2 class="mb-4 text-sm title-font text-gray-500 tracking-widest">{{ $product->category->name}}</h2>
+                         <h1 class="mb-4 text-gray-900 text-3xl title-font font-medium">{{ $product->name }}</h1>
+                         <p class="mb-4 leading-relaxed">{{ $product->information }}</p>
+                     {{-- 金額、数量、カートボタン表示 items-center(縦方向中央揃え)--}}
+                         <div class="flex justify-around items-center">
+                            {{-- number_formatで価格にコロンいれる --}}
+                            <div>
+                              <span class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }}</span><span class="text-sm text-gray-700">円(税込)</span>
+                            </div>
+                            <div class="flex items-center">
+                              {{-- 数量表示 --}}
+                              <span class="mr-3">数量</span>
+                              <div class="relative">
+                                  <select class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-pink-200 focus:border-pink-500 text-base pl-3 pr-10">
+                                    <option>SM</option>
+                                     <option>M</option>
+                                     <option>L</option>
+                                     <option>XL</option>
+                                  </select>
+                              </div>
+                            </div>
+                            <button class="flex ml-auto text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">カートに入れる</button>
+                         </div>
+
+                     </div>
+                 </div>
+
               </div>
           </div>
       </div>
