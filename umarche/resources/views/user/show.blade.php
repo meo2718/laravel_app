7c6ba4b5308ecd5a594db070604f38d18c489e51
meo2718 <agv1291@gmail.com>
2023-01-05 23:39:05 +0900

    [Add]shop情報表示modal　#54

--- umarche/resources/views/user/show.blade.php
+++ umarche/resources/views/user/show.blade.php
@@ -105,11 +105,33 @@
                  </div>
                  <div class="mb-4 text-center">
                     {{-- type="button"とすることでpost通信にしない --}}
-                    <button type="button" class="text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">ショップの詳細を見る</button>
+                    {{-- data-micromodal-trigger="modal-1" href='javascript:;'→modal-1を開くボタン --}}
+                    <button data-micromodal-trigger="modal-1" href='javascript:;' type="button" class="text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">ショップの詳細を見る</button>
                  </div>
               </div>
           </div>
       </div>
+  </div>
+{{-- modalテンプレートgithubから --}}
+  <div class="modal micromodal-slide" id="modal-1" aria-hidden="true">
+    <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+      <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
+        <header class="modal__header">
+          <h2 class="text-xl text-gray-700" id="modal-1-title">
+            {{ $product->shop->name }}
+          </h2>
+          <button type="button" class="modal__close" aria-label="Close modal" data-micromodal-close></button>
+        </header>
+        <main class="modal__content" id="modal-1-content">
+          <p>
+            {{ $product->shop->information }}
+          </p>
+        </main>
+        <footer class="modal__footer">
+          <button type="button" class="modal__btn" data-micromodal-close aria-label="Close this dialog window">閉じる</button>
+        </footer>
+      </div>
+    </div>
   </div>
   {{-- sliderの読み込み --}}
   <script src="{{ mix('js/swiper.js') }}"> </script>
