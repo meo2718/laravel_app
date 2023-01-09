f046cbfb26d28465bfff7090555b1632383b7033
meo2718 <agv1291@gmail.com>
2023-01-10 02:43:50 +0900

    [Add]Cart add index #56

--- umarche/routes/web.php
+++ umarche/routes/web.php
@@ -35,6 +35,7 @@ Route::middleware('auth:users')->group(function(){
 Route::prefix('cart')-> 
     middleware('auth:users')->group(function(){
         Route::post('add', [CartController::class, 'add'])->name('cart.add');
+        Route::get('/', [CartController::class, 'index'])->name('cart.index');
 });
 
 //googleログインのルート
