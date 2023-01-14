5ffef509216fce8567aca172edeb377ac7bdef21
meo2718 <agv1291@gmail.com>
2023-01-14 23:37:02 +0900

    [Clean]共通クエリ制約の定義 #60

--- umarche/app/Models/Product.php
+++ umarche/app/Models/Product.php
@@ -9,6 +9,7 @@ use App\Models\Image;
 use App\Models\Stock;
 use App\Models\SecondaryCategory;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class Product extends Model
 {
@@ -65,5 +66,26 @@ class Product extends Model
     {
         return $this->belongsToMany(User::class,'carts')
         ->withPivot(['id', 'quantity']);
+    }
+
+    public function scopeAvailableItems($query)
+    {
+        $stocks = DB::table('t_stocks') 
+        ->select('product_id', DB::raw('sum(quantity) as quantity')) 
+        ->groupBy('product_id') 
+        ->having('quantity', '>', 1);
+        return $query
+
+        ->joinSub($stocks, 'stock', function($join){
+        //さらにproductとshopテーブルをjoinでつなげる
+        $join->on('products.id', '=', 'stock.product_id'); })
+        ->join('shops', 'products.shop_id', '=', 'shops.id')
+        ->join('secondary_categories', 'products.secondary_category_id', '=', 'secondary_categories.id')
+        ->join('images as image1', 'products.image1', '=', 'image1.id')
+        //shopsとproductsのis_sellingがtrueならgetする
+        ->where('shops.is_selling', true)
+        ->where('products.is_selling', true)
+        ->select('products.id as id', 'products.name as name', 'products.price' ,'products.sort_order as sort_order',
+        'products.information', 'secondary_categories.name as category' ,'image1.filename as filename');
     }
 }
